e3e290719e7776f6b6f08aa0b03a284d8e394b80
Alexey <lunizz@gmail.com>
2024-09-08 21:23:37 +0300

    Branch2

---
+++ spring-proj/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
