a7322353cf3c488e2113a30acccaffded9a5ecab
Alexey <lunizz@gmail.com>
2024-09-17 11:22:43 +0300

    Branch16

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import Dasboard from './component/Dasboard';
+import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './component/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Header /> 
-      <Dasboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> 
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+      
+    </Router>
   );
 }
 
---
+++ ppmtool-react-client/src/component/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div><h1>AddProject Form</h1></div>
+    )
+  }
+}
+
+export default AddProject;
